5ea96e203e128117cf96b8fd6c08c99feb9bb83f
abc90155 <abc90155@gmail.com>
2023-03-02 17:34:17 +0800

    devise login
    use Gem devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
-#email:string
-#password_digest:string
-#
-#password:string virtual
-#password_conformation:string virtual
-
 class User < ApplicationRecord
-    has_secure_password
-
-    validates :email, presence:true
+  #另外加的 不加的話指令下去抓不到devise
+  extend Devise::Models
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   #resources :comments
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "tasks#index"
+  root :to => "tasks#index"
 
   get "tasks", to: "tasks#index"
   get "sign_up", to: "registrations#new"
